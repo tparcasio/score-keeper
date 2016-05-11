2198d7c47786bf2f1264c5e0d67804205ad6f9fe
Tiffany Parcasio <tparcasio@us00972-ml.corp.qc>
2016-05-10 18:38:23 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+      var score = 0;
+
+
+      $(document).on("click", "#increase-5", function() {
+          $("#score").html((score += 5) + " Points");
+      });
+
+      $(document).on("click", "#decrease-5", function() {
+          if (score >=5) {
+            $("#score").html((score -= 5) + " Points");
+          } else {
+            alert("Don't be negative!");
+          }
+      });
+
+      $(document).on("click", "#submit-custom-score", function() {
+          var entry = parseInt($("#custom-score").val());
+
+          score = entry;
+
+          if (entry >= 0) {
+            $("#score").html(entry + " Points");
+          } else {
+            alert("Don't be negative!");
+          }
+
+      });
+
+
+});
