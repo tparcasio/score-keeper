ce3edb4955f6b7056e8fbca38dd6903953997575
Tiffany Parcasio <tparcasio@us00972-ml.corp.qc>
2016-05-10 19:30:19 -0700

    added comments

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ $(document).ready(function() {
 
       var score = 0;
 
-
+      //Increase 5 on click
       $(document).on("click", "#increase-5", function() {
           $("#score").html((score += 5) + " Points");
       });
 
+      //Decrease 5 on click
       $(document).on("click", "#decrease-5", function() {
           if (score >=5) {
             $("#score").html((score -= 5) + " Points");
@@ -15,6 +16,7 @@ $(document).ready(function() {
           }
       });
 
+      //set score on click
       $(document).on("click", "#submit-custom-score", function() {
           var entry = parseInt($("#custom-score").val());
 
